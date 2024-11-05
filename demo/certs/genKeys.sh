312b72d8088b9ce8cb7d7fb2d395b88446b95880
Brett Nicholas <7547222+bigbrett@users.noreply.github.com>
2024-11-05 11:26:27 -0700

    add keys

--- demo/certs/genKeys.sh
+++ demo/certs/genKeys.sh
@@ -10,3 +10,9 @@ openssl ec -in bob-ecc256-key.pem -outform DER -out bob-ecc256-key.der
 openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out rsa-2048-key.pem
 openssl rsa -in rsa-2048-key.pem -outform DER -out rsa-2048-key.der
 
+# TODO: Curve25519 Keys
+# Either need to modify examples to split out public/private operations, or modify wolfHSM to be able to recove public keys on deserialization of private only.
+#openssl genpkey -algorithm X25519 -out alice-curve25519-key.pem
+#openssl genpkey -algorithm X25519 -out bob-curve25519-key.pem
+#openssl pkey -in alice-curve25519-key.pem -outform DER -out alice-curve25519-key.der
+#openssl pkey -in bob-curve25519-key.pem -outform DER -out bob-curve25519-key.der
